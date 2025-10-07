06e3acb767aae13941cddd63fe89401ea02c6b82
mikhou <buisnessamirdabou@gmail.com>
2025-10-07 13:19:25 +0100

    v0.1

--- calory.py
+++ calory.py
@@ -1,5 +1,5 @@
 def calculate_bmr(weight, height, age, gender='male'):
-    if gender == 'male':
+    if gender == 'maleeeeee':
         return 10 * weight + 6.25 * height - 5 * age + 5
     else:
         return 10 * weight + 6.25 * height - 5 * age - 161
